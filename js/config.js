823193240c9694a1da7ef92323e45be0f169499b
Xiao-mz <mazx528@gmail.com>
2026-06-07 15:29:04 +0800

    Configure Google Form submission link

---
+++ js/config.js
@@ -0,0 +1,3 @@
+window.LITERATURE_BROWSER_CONFIG = {
+  submissionFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLScR9_4CzXCm2h6boLzwp99nM5Qnw-Ran8CtoK4q5gZk21F8Xw/viewform?usp=publish-editor",
+};
